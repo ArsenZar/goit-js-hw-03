13f8ca8768a82c7403e5bd334360dbc33fbf496c
ArsenZar <arsenzar@gmail.com>
2025-03-19 14:47:04 +0200

    practice-5 (take keys and values in object)

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,27 @@
 
+/* practice-5 */
+/* this example shows how to get keys and values from an object if we don`t know the keys and values */
 
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+
+const keys = [];
+const values = [];
+
+for (const key in apartment){
+  keys.push(key);
+  values.push(apartment[key]);
+}
+
+console.log(keys);
+console.log(values);
+
+
+/* practice-4 */
+/*
 const emailInputName = "email";
 const passwordInputName = "password";
 
@@ -7,7 +29,7 @@ const credentials = {
   [emailInputName]: "henry.carter@aptmail.com",
   [passwordInputName]: "jqueryismyjam",
 };
-
+*/
 /*
 --- practice
 
