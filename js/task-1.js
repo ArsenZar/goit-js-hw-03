4fac5e8ba58724d4bbc5f28ce5869a17b7f33e81
ArsenZar <arsenzar@gmail.com>
2025-03-19 13:53:52 +0200

    practice-2

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,22 @@
+const book = {
+  title: "The Last Kingdom",
+  author: "Bernard Cornwell",
+  genres: ["historical prose", "adventure"],
+  isPublic: true,
+  rating: 8.38,
+};
 
+book.pageCount = 836;
+book.originalLanguage = "en";
+book.translations = ["ua", "ru"];
+book.price = {
+  hardcover: 39,
+  softcover: 29,
+};
 
+console.log(book.pageCount); // 836
+console.log(book.originalLanguage); // "en"
+console.log(book.translations); // ["ua", "ru"]
 
 
 
